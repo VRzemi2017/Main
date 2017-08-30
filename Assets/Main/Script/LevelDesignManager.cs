3635c4c87b0bf8648e6e163da02d22793e1f458b
kerrot <kerrot>
2017-08-30 18:28:04 +0900

    Random Level Setting

---
+++ Assets/Main/Script/LevelDesignManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDesignManager : MonoBehaviour {
+    [SerializeField] List<LevelSetting> setting = new List<LevelSetting>();
+    [SerializeField] List<RandomTable> randomTable = new List<RandomTable>();
+
+    [System.Serializable]
+    public struct LevelSetting
+    {
+        [TextArea(3, 10)]
+        public string Memo;
+        public GameObject[] StartPositionSet;
+        public EnemyGanrator EnemySetting;
+        public GemGanerator GemSetting;
+    }
+
+    [System.Serializable]
+    public struct RandomTable
+    {
+        public int[] Sequence;
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    [ContextMenu("GenerateRandomTable")]
+    public void GenerateRandomTable()
+    {
+        List<int> table = new List<int>();
+
+        for (int i = 0; i < setting.Count; ++i)
+        {
+            int tmp = 0;
+            do
+            {
+                tmp = UnityEngine.Random.Range(0, setting.Count);
+            } while (table.Contains(tmp));
+
+            table.Add(tmp);
+        }
+
+        randomTable.Add(new RandomTable() { Sequence = table.ToArray()});
+    }
+
+    public System.Nullable<LevelSetting> GetLevelSetting(int index)
+    {
+        if (index >= 0 && index < setting.Count)
+        {
+            return setting[index];
+        }
+
+        return null;
+    }
+
+    public RandomTable RandomPickTable()
+    {
+        if (randomTable.Count == 0)
+        {
+            GenerateRandomTable();
+        }
+
+        return randomTable[UnityEngine.Random.Range(0, randomTable.Count)];
+    }
+}
--- Assets/Main/Script/MainScene.cs
+++ Assets/Main/Script/MainScene.cs
@@ -7,13 +7,34 @@ using UniRx.Triggers;
 
 public class MainScene : SceneBase
 {
+    static System.Nullable<LevelDesignManager.RandomTable> table;
+    static int index = -1;
+
+    LevelDesignManager lv;
+
     private void Awake()
     {
         MainManager.ChangeState(MainManager.GameState.GAME_INIT);
+
+        if (table == null)
+        {
+            lv = GameObject.FindObjectOfType<LevelDesignManager>();
+            if (lv)
+            {
+                table = lv.RandomPickTable();
+            }
+        }
+
+        GemGanerator[] gems = GameObject.FindObjectsOfType<GemGanerator>();
+        gems.ToList().ForEach(g => g.gameObject.SetActive(false));
+
+        EnemyGanrator[] enemies = GameObject.FindObjectsOfType<EnemyGanrator>();
+        enemies.ToList().ForEach(e => e.gameObject.SetActive(false));
     }
 
     void Start () 
     {
+        InitLevelSetting();
         InitPlayerPosition();
         MainManager.ChangeState(MainManager.GameState.GAME_NETWORK);
 
@@ -31,5 +52,25 @@ public class MainScene : SceneBase
                 }
             }
         });
+    }
+
+    void InitLevelSetting()
+    {
+        if (!lv)
+        {
+            lv = GameObject.FindObjectOfType<LevelDesignManager>();
+        }
+
+        if (lv)
+        {
+            index = ++index % table.Value.Sequence.Length;
+            System.Nullable<LevelDesignManager.LevelSetting> setting = lv.GetLevelSetting(table.Value.Sequence[index]);
+            if (setting != null)
+            {
+                setting.Value.GemSetting.gameObject.SetActive(true);
+                setting.Value.EnemySetting.gameObject.SetActive(true);
+                startPosition = setting.Value.StartPositionSet.ToList();
+            }
+        }
     }
 }
