69cc17242ce6c5c73b56d9f4953a2ff1034836ae
kerrot <kerrot>
2017-09-02 17:09:31 +0900

    Result Test

---
+++ Assets/Main/Script/ResultManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour {
+
+    [SerializeField] List<ScoreSetting> score = new List<ScoreSetting>();
+    [SerializeField] List<CommentSetting> comment = new List<CommentSetting>();
+
+    public enum ScoreType
+    {
+        SCORE_D,
+        SCORE_C,
+        SCORE_B,
+        SCORE_A,
+        SCORE_S,
+        SCORE_SS,
+    }
+
+    public enum ConditionType
+    {
+        CONDI_GEM,
+        CONDI_DAMEGE,
+        CONDI_TELEPORT,
+        CONDI_SPOT,
+    }
+
+    public enum OPType
+    {
+        OP_EQUAL,
+        OP_NOT_EQUAL,
+        OP_GREATER,
+        OP_GEQUAL,
+        OP_LESS,
+        OP_LEQUAL,
+    }
+
+    [System.Serializable]
+    public struct ScoreSetting
+    {
+        public ScoreType Score;
+        public ScoreCondition[] conditions;
+    }
+
+    [System.Serializable]
+    public struct ScoreCondition
+    {
+        public ConditionType Type;
+        public OPType OP;
+        public int Param;
+    }
+
+    [System.Serializable]
+    public struct CommentSetting
+    {
+        [TextArea(3, 10)]
+        public string Comment;
+        public ScoreCondition[] conditions;
+    }
+}
---
+++ Assets/Main/Script/SpotControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotControl : MonoBehaviour {
+    [SerializeField] int ID;
+
+    private bool reached;
+    public bool IsReached { get { return reached; } }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        reached = true;
+        GetComponent<Collider>().enabled = false;
+    }
+}
