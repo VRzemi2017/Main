320cf059f970d65618e2940315e7da2a1f59d133
kerrot <kerrot>
2017-09-09 23:02:42 +0900

    auto prefab generate

---
+++ Assets/Main/Script/PrefabObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PrefabObject : MonoBehaviour {
+    [SerializeField] GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+        prefabs.ToList().ForEach(p =>
+        {
+            GameObject obj = Instantiate(p);
+            p.transform.parent = transform;
+            p.transform.localPosition = Vector3.zero;
+            p.transform.localRotation = Quaternion.identity;
+            p.transform.localScale = Vector3.one;
+        });
+	}
+}
