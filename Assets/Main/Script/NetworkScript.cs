89c04313419d53a84df242dd524f9c10b906bffa
kerrot <kerrot>
2017-09-09 21:21:01 +0900

    Network Script Test

---
+++ Assets/Main/Script/NetworkScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(MonobitEngine.MonobitView))]
+public class NetworkScript : MonoBehaviour {
+
+    [SerializeField] MonoBehaviour[] scripts;
+
+    private void Awake()
+    {
+        scripts.ToList().ForEach(s => s.enabled = false);
+    }
+}
