933df6de4c621bbe1e6350b0faf8465ff5097227
kerrot <kerrot>
2017-09-11 19:22:09 +0900

    Fix Info

---
+++ Assets/Component/Info/MainTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int tmp = (int)MainManager.CurrentState;
+
+            tmp = (tmp + 1) % (int)(MainManager.GameState.GAME_STATE_MAX);
+
+            MainManager.ChangeState((MainManager.GameState)tmp);
+
+            Debug.Log(MainManager.CurrentState);
+        }
+    }
+}
